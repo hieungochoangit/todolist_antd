c1f1882904d4b2d6c996e490163de083c40e25d6
hieuhn <hieungochoangit@gmail.com>
2021-12-24 09:39:26 +0700

    Fix status filters

--- src/store/selector.js
+++ src/store/selector.js
@@ -3,15 +3,23 @@ export const todoSelector = (state) => {
   const newTodo = newState.todos.filter(todo => {
 
     if (state.filters.status === 'all') {
-      return todo.name.includes(state.filters.search) && !state.filters.level.includes(todo.level);
+      return (
+        todo.name.includes(state.filters.search) &&
+        (state.filters.level.length > 0) ?
+        state.filters.level.includes(todo.level) : true
+      );
     }
 
     if (state.filters.status === 'completed') {
-      return todo.name.includes(state.filters.search) && todo.isCompleted === true;
+      return todo.name.includes(state.filters.search) &&
+      (state.filters.level.length > 0) ?
+      state.filters.level.includes(todo.level) : true && todo.isCompleted === true;
     }
 
     if (state.filters.status === 'pending') {
-      return todo.name.includes(state.filters.search) && todo.isCompleted === false;
+      return todo.name.includes(state.filters.search) &&
+      (state.filters.level.length > 0) ?
+      state.filters.level.includes(todo.level) : true && todo.isCompleted === false;
     }
 
     return todo.name.includes(state.filters.search);
